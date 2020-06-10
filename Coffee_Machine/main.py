4e8542e8797ea561c8bdc5658e08f97b79bcc7ba
akrason <olakrason1999@gmail.com>
2020-06-10 18:00:30 +0200

    Update

--- Coffee_Machine/main.py
+++ Coffee_Machine/main.py
@@ -1,6 +1,13 @@
 from coffeemachine import CoffeeMachine
 
-coffee_machine = CoffeeMachine([400, 150, 75, 10, 500])
 
-while coffee_machine.run:
-    coffee_machine.action()
+def main(args):
+    coffee_machine = CoffeeMachine([400, 150, 75, 10, 500])
+
+    while coffee_machine.run:
+        coffee_machine.action()
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
